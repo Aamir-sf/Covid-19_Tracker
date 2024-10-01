b8c9289344cc00cb40774098a37be46b45469228
Abu Aamir <124289209+Aamir-sf@users.noreply.github.com>
2024-10-01 15:53:53 +0530

    my first  commit

---
+++ lib/View/SplashScreen.dart
@@ -0,0 +1,76 @@
+import 'dart:async';
+
+import 'package:covid_19/View/world_states.dart';
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller =
+      AnimationController(duration: const Duration(seconds: 3), vsync: this)
+        ..repeat();
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
+  }
+
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Timer(
+        const Duration(seconds: 5),
+        () => Navigator.push(context,
+            MaterialPageRoute(builder: (context) => WorldStatesScreen())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          AnimatedBuilder(
+              animation: _controller,
+              child: Container(
+                height: 200,
+                width: 200,
+                child: const Center(
+                  child: Image(image: AssetImage('images/virus.png')),
+                ),
+              ),
+              builder: (BuildContext context, Widget? child) {
+                return Transform.rotate(
+                  angle: _controller.value * 2.0 * math.pi,
+                  child: child,
+                );
+              }),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * .08,
+          ),
+          const Align(
+            alignment: Alignment.center,
+            child: Text(
+              'Covid-19\nTracker App\nBy\n~Aamir~',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/View/world_states.dart
@@ -0,0 +1,183 @@
+import 'package:covid_19/Model/WorldStatesModel.dart';
+import 'package:covid_19/View/countries_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:pie_chart/pie_chart.dart';
+
+import '../Services/states_services.dart';
+
+class WorldStatesScreen extends StatefulWidget {
+  const WorldStatesScreen({super.key});
+
+  @override
+  State<WorldStatesScreen> createState() => _WorldStatesScreenState();
+}
+
+class _WorldStatesScreenState extends State<WorldStatesScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller =
+      AnimationController(duration: const Duration(microseconds: 1050), vsync: this)
+        ..repeat();
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
+  }
+
+  final colorList = const <Color>[
+    Color(0xff4285F4),
+    Color(0xff1aa260),
+    Color(0xffde5246),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    StatesServices statesServices = StatesServices();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: SafeArea(
+              child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * .01,
+              ),
+              FutureBuilder(
+                  future: statesServices.fecthWorldStatesRecords(),
+                  builder: (context, AsyncSnapshot<WorldStatesModel> snapshot) {
+                    if (!snapshot.hasData) {
+                      return Expanded(
+                        flex: 1,
+                        child: SpinKitFadingCircle(
+                          color: Colors.white,
+                          size: 50.0,
+                          controller: _controller,
+                        ),
+                      );
+                    } else {
+                      return Column(
+                        children: [
+                          PieChart(
+                            dataMap: {
+                              "Total":
+                                  double.parse(snapshot.data!.cases!.toString()),
+                              "Recoverd": double.parse(
+                                  snapshot.data!.recovered!.toString()),
+                              "Deaths":
+                                  double.parse(snapshot.data!.deaths!.toString()),
+                            },
+                            animationDuration: Duration(milliseconds: 1200),
+                            chartLegendSpacing: 32,
+                            chartRadius: MediaQuery.of(context).size.width / 3.2,
+                            colorList: colorList,
+                            initialAngleInDegree: 0,
+                            chartType: ChartType.ring,
+                            ringStrokeWidth: 25,
+                            legendOptions: const LegendOptions(
+                              showLegendsInRow: false,
+                              legendPosition: LegendPosition.left,
+                              showLegends: true,
+                              legendTextStyle: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            chartValuesOptions: const ChartValuesOptions(
+                              showChartValueBackground: true,
+                              showChartValues: true,
+                              showChartValuesInPercentage: true,
+                              showChartValuesOutside: true,
+                              decimalPlaces: 1,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical:
+                                    MediaQuery.of(context).size.height * .06),
+                            child: Card(
+                              child: Column(
+                                children: [
+                                  ReusableRow(
+                                      title: 'Total Cases',
+                                      value: snapshot.data!.cases.toString()),
+                                  ReusableRow(
+                                      title: 'Deaths',
+                                      value: snapshot.data!.deaths.toString()),
+                                  ReusableRow(
+                                      title: 'Recovered',
+                                      value: snapshot.data!.recovered.toString()),
+                                  ReusableRow(
+                                      title: 'Active',
+                                      value: snapshot.data!.active.toString()),
+                                  ReusableRow(
+                                      title: 'Critical',
+                                      value: snapshot.data!.critical.toString()),
+                                  ReusableRow(
+                                      title: 'Today Deaths',
+                                      value:
+                                          snapshot.data!.todayDeaths.toString()),
+                                  ReusableRow(
+                                      title: 'Today Recovered',
+                                      value: snapshot.data!.todayRecovered
+                                          .toString()),
+                                ],
+                              ),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          CountriesListScreen()));
+                            },
+                            child: Container(
+                              height: 50,
+                              decoration: BoxDecoration(
+                                  color: const Color(0xff1aa260),
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: const Center(
+                                child: Text('Track Countries'),
+                              ),
+                            ),
+                          )
+                        ],
+                      );
+                    }
+                  }),
+            ],
+          )),
+        ),
+      ),
+    );
+  }
+}
+
+class ReusableRow extends StatelessWidget {
+  String title, value;
+  ReusableRow({super.key, required this.title, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(title),
+              Text(value),
+            ],
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Divider()
+        ],
+      ),
+    );
+  }
+}
